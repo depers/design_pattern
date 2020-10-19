62fb2d5a334685f897ea8401ad3cf32d1a60a174
depers <dev_fengxiao@163.com>
2020-10-19 22:00:41 +0800

    8.1 原型模式coding

---
+++ src/main/java/cn/bravedawn/design/pattern/creational/prototype/Mail.java
@@ -0,0 +1,51 @@
+package cn.bravedawn.design.pattern.creational.prototype;
+
+/**
+ * @author : depers
+ * @program : design_pattern
+ * @description: 邮件
+ * @date : Created in 2020/10/19 21:45
+ */
+public class Mail {
+
+    private String name;
+    private String emailAddress;
+    private String content;
+
+    public Mail(){
+        System.out.println("Mail Class Constructor");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Mail{" +
+                "name='" + name + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/cn/bravedawn/design/pattern/creational/prototype/MailUtil.java
@@ -0,0 +1,21 @@
+package cn.bravedawn.design.pattern.creational.prototype;
+
+import java.text.MessageFormat;
+
+/**
+ * @author : depers
+ * @program : design_pattern
+ * @description: 邮件工具
+ * @date : Created in 2020/10/19 21:48
+ */
+public class MailUtil {
+
+    public static void sendMail(Mail mail){
+        String outputContent = "向{0}同学，邮件地址:{1}，邮件内容{2}发送邮件";
+        System.out.println(MessageFormat.format(outputContent, mail.getName(), mail.getEmailAddress(), mail.getContent()));
+    }
+
+    public static void saveOriginMailRecord(Mail mail){
+        System.out.println("存储originMail记录，originMail: " + mail.getContent());
+    }
+}
